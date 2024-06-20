d87f7ad4acbd3f65d6fbbc5f56197795da6d5b32
Pedram <pedisdi@gmail.com>
2024-06-20 12:41:03 +0330

    no charge

--- src/order/urls.py
+++ src/order/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('cart/', views.cart, name='cart'),
     path('order/', views.order, name='order'),
-    path('submit-order/<str:selected_category>/', views.submit_order, name='submit-order'),
+    path('submit-order/<str:selected_category>/<int:menu_item_id>/',
+         views.submit_order,
+         name='submit-order'
+         ),
 ]
--- src/order/views.py
+++ src/order/views.py
@@ -1,7 +1,10 @@
 import datetime
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Order, OrderItem
+from user.models import Customer
+
+from menu.models import MenuItem
 
 
 def cart(request):
@@ -17,7 +20,15 @@ def order(request):
     return render(request, 'order/order.html', {})
 
 
-def submit_order(request, selected_category=None):
-    print(request.COOKIES.get('username'))
+def submit_order(request, selected_category=None, menu_item_id=None):
+    print(request.COOKIES.get('username'), selected_category, menu_item_id)
+    customer_id = get_object_or_404(Customer, username=request.COOKIES.get('username')).id
+    # page not found cases are:
+    # 1. the username is for a staff instead of a customer!
+    # 2. the cookie is expired
 
+    selected_menu_item = MenuItem.objects.get(id=menu_item_id)
+    print(selected_menu_item)
+    unpaid_order = Order.get_unpaid_order(customer_id)
+    OrderItem.objects.create()
     return redirect('menu', selected_category)

60ba882bb16ce227a5397cb5632821d869b0da08
Pedram <pedisdi@gmail.com>
2024-06-26 12:49:42 +0330

    Add 'manage' button to header and some bug fix

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 
@@ -6,7 +6,9 @@ from . import forms
 
 
 def login_view(request):
-    # form = forms.LoginForm()
+    if request.user.is_authenticated:
+        return redirect('website-home')
+
     if request.method == 'POST':
         form = forms.LoginForm(request.POST)
         if form.is_valid():
@@ -19,9 +21,12 @@ def login_view(request):
                 return redirect('website-home')
             # else:
             #     return HttpResponse('invalid login')
+        else:
+            return redirect('accounts:login')
     return render(request, 'accounts/login.html')  # context={'form': form})
 
 
+@login_required
 def logout_view(request):
     logout(request)
     return redirect('website-home')

e19769c67e3545d2e100a1f50289811c42f75680
Arma <armaqanalibabaei@gmail.com>
2024-06-26 12:53:58 +0330

    Feature:Added MenuItem  form and Category form

--- src/user/forms.py
+++ src/user/forms.py
@@ -1,1 +1,36 @@
+from django import forms
+from django.forms import ModelForm
+from .models import Staff
+from menu.models import MenuCategory
 
+from menu.models import MenuItem
+
+
+# class StaffForm(ModelForm):
+#     class Meta:
+#         model = Staff
+#         fields = '__all__'
+
+
+class AddCategoryForm(forms.ModelForm):
+    class Meta:
+        model = MenuCategory
+        fields = '__all__'
+        widgets = {
+            'label': forms.TextInput()
+        }
+
+
+class AddMenuItem(forms.ModelForm):
+    class Meta:
+        model = MenuItem
+        fields = '__all__'
+        widgets = {
+            'food_name': forms.TextInput(),
+            'description': forms.Textarea(),
+            'price': forms.NumberInput(),
+            'discount': forms.NumberInput(),
+            'inventory': forms.NumberInput(),
+            'image': forms.FileInput(),
+            'menu_category': forms.Select(),
+        }
--- src/user/urls.py
+++ src/user/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
 
     path('staff/', views.all_staffs_view, name='all-staffs'),
     path('staff/<str:staff_username>/', views.staff_profile, name='staff-profile'),
+    path('custom/', views.staff_menu_categories, name='staff_menu_categories'),
+    path('menu_item/', views.add_menu_item, name='add_menu_item'),
 ]
+
+

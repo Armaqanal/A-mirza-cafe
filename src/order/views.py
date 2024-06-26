b284f4ffbfa7b55b39e45365182cf37dc4d6db9c
ZEYNAB <zibasoltani73@gmail.com>
2024-06-26 21:32:51 +0330

    merge develop into ziba

--- src/order/views.py
+++ src/order/views.py
@@ -21,52 +21,52 @@ def order(request):
     return render(request, 'order/order.html', {})
 
 
-def add_menu_item_to_cart(request, selected_category=None, menu_item_id=None):
-    print(request.COOKIES.get('username'), selected_category, menu_item_id)
-    username = request.COOKIES.get('username')
-    customer = get_object_or_404(Customer, username=username)
-    # page not found cases are:
-    # 1. the username is for a staff instead of a customer!
-    # 2. the cookie is expired
-
-    selected_menu_item = MenuItem.objects.get(id=menu_item_id)
-    print(selected_menu_item)
-    unpaid_order = Order.get_unpaid_order(customer_id=customer.id)
-
-    # check if this item has been already selected for the 'unpaid_order',
-    # if it exits get it and if it doesn't create it
-    order_item = OrderItem.objects.filter(order=unpaid_order).get_or_create(menu_item=selected_menu_item,
-                                                                            order=unpaid_order, quantity=1)
-
-    return redirect('menu', selected_category)
-
-
 # def add_menu_item_to_cart(request, selected_category=None, menu_item_id=None):
 #     print(request.COOKIES.get('username'), selected_category, menu_item_id)
-#
-#     username = request.session.get('username')
+#     username = request.COOKIES.get('username')
 #     customer = get_object_or_404(Customer, username=username)
+#     # page not found cases are:
+#     # 1. the username is for a staff instead of a customer!
+#     # 2. the cookie is expired
 #
 #     selected_menu_item = MenuItem.objects.get(id=menu_item_id)
 #     print(selected_menu_item)
+#     unpaid_order = Order.get_unpaid_order(customer_id=customer.id)
 #
-#     # Find the online customer
-#     online_customer = Customer.objects.get(is_online=True)
-#
-#     # Find the  online unpaid order
-#     unpaid_order = Order.objects.filter(customer=online_customer, is_paid=False).first()
-#
-#     order_item, created = OrderItem.objects.get_or_create(
-#         order=unpaid_order,
-#         menu_item=selected_menu_item,
-#         defaults={'quantity': 1}
-#     )
-#
-#     if not created:
-#         order_item.quantity += 1
-#         order_item.save()
+#     # check if this item has been already selected for the 'unpaid_order',
+#     # if it exits get it and if it doesn't create it
+#     order_item = OrderItem.objects.filter(order=unpaid_order).get_or_create(menu_item=selected_menu_item,
+#                                                                             order=unpaid_order, quantity=1)
 #
 #     return redirect('menu', selected_category)
+
+
+def add_menu_item_to_cart(request, selected_category=None, menu_item_id=None):
+    print(request.COOKIES.get('username'), selected_category, menu_item_id)
+
+    username = request.session.get('username')
+    customer = get_object_or_404(Customer, username=username)
+
+    selected_menu_item = MenuItem.objects.get(id=menu_item_id)
+    print(selected_menu_item)
+
+    # Find the online customer
+    online_customer = Customer.objects.get(is_online=True)
+
+    # Find the  online unpaid order
+    unpaid_order = Order.objects.filter(customer=online_customer, is_paid=False).first()
+
+    order_item, created = OrderItem.objects.get_or_create(
+        order=unpaid_order,
+        menu_item=selected_menu_item,
+        defaults={'quantity': 1}
+    )
+
+    if not created:
+        order_item.quantity += 1
+        order_item.save()
+
+    return redirect('menu', selected_category)
 
 
 def customer_orders_view(request):

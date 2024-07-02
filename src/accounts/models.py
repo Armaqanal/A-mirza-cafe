5bbaf627fa506651b5edcf245703c44da39967ff
ZEYNAB <zibasoltani73@gmail.com>
2024-07-02 16:03:16 +0330

    models.py

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, phone_number, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+        if not phone_number:
+            raise ValueError('Users must have a phone number')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            phone_number=phone_number,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(verbose_name="email", max_length=60, unique=True)
+    phone_number = models.CharField(verbose_name="phone number", max_length=50, unique=True)
+    password = models.CharField(verbose_name="password", max_length=60)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['phone_number']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.phone_number

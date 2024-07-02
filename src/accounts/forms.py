dae92c68ce5ad70caf32febba9d6f0f39bc0951a
ZEYNAB <zibasoltani73@gmail.com>
2024-07-02 13:26:16 +0330

    forms.py(user_login)

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -1,16 +1,38 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate
 from django import forms
-
-#
-# class UserLoginForm(forms.ModelForm):
-#     class Meta:
-#         model = get_user_model()
-#         fields = ("username", "password")
-#         widgets = {
-#             "password": forms.PasswordInput(), }
-# authentication/forms.py
 
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=63)
     password = forms.CharField(max_length=63, widget=forms.PasswordInput)
+
+
+class UserLoginForm(forms.Form):
+    email = forms.EmailField(max_length=60, widget=forms.EmailInput)
+    phone_number = forms.CharField(max_length=15, widget=forms.NumberInput)
+    password = forms.CharField(widget=forms.PasswordInput)
+    gender = forms.ChoiceField(choices=[('Man', 'Female')])
+    age = forms.IntegerField(widget=forms.NumberInput)
+    address = forms.CharField(max_length=100)
+
+    # def clean(self):
+    #     cleaned_data = super().clean()
+    #     email = cleaned_data.get('email')
+    #     password = cleaned_data.get('password')
+    #     phone_number = cleaned_data.get('phone_number')
+    #     # gender = cleaned_data.get('gender')
+    #     # age = cleaned_data.get('age')
+    #     # address = cleaned_data.get('address')
+    #     user = authenticate(username=email, phone_number=phone_number, password=password)
+    #     if user is not None:
+    #         return user
+    #     else:
+    #         raise forms.ValidationError('Invalid login credentials')
+
+
+
+
+
+
+
+

6f8ea33ac3c04a2c02b2f6de5da3e00d0cf5a330
ZEYNAB <zibasoltani73@gmail.com>
2024-07-02 11:46:17 +0330

    user_login

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('signup/', signup_view, name='signup'),
+    path('login/', user_login_view, name='login')
 ]

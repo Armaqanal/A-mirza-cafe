5ee2059c41b79c52e9e7c1b108158cb1a20813b6
ZEYNAB <zibasoltani73@gmail.com>
2024-07-04 00:41:19 +0330

    forms.py

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -1,37 +1,23 @@
 # from django.contrib.auth import authenticate
 from django import forms
 
+from .models import User
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=63)
     password = forms.CharField(max_length=63, widget=forms.PasswordInput)
 
 
-class UserLoginForm(forms.Form):
+class UserLoginForm(forms.ModelForm):
     username = forms.CharField(max_length=100)
     password = forms.CharField(widget=forms.PasswordInput, max_length=63)
+    email = forms.EmailField(max_length=90)
+    phone_number = forms.CharField(max_length=100)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email', 'phone_number']
+        widgets = {
+            'password': forms.PasswordInput()
+        }

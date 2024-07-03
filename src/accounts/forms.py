ed3e809fec2b4d180389bf0d6d175594628327e7
ZEYNAB <zibasoltani73@gmail.com>
2024-07-03 15:43:04 +0330

    forms.py

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -8,26 +8,25 @@ class LoginForm(forms.Form):
 
 
 class UserLoginForm(forms.Form):
-    email = forms.EmailField(max_length=60, widget=forms.EmailInput)
-    phone_number = forms.CharField(max_length=15, widget=forms.NumberInput)
-    password = forms.CharField(widget=forms.PasswordInput)
-    gender = forms.ChoiceField(choices=[('Man', 'Female')])
-    age = forms.IntegerField(widget=forms.NumberInput)
-    address = forms.CharField(max_length=100)
+    username = forms.CharField(max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput, max_length=63)
 
-    # def clean(self):
-    #     cleaned_data = super().clean()
-    #     email = cleaned_data.get('email')
-    #     password = cleaned_data.get('password')
-    #     phone_number = cleaned_data.get('phone_number')
-    #     # gender = cleaned_data.get('gender')
-    #     # age = cleaned_data.get('age')
-    #     # address = cleaned_data.get('address')
-    #     user = authenticate(username=email, phone_number=phone_number, password=password)
-    #     if user is not None:
-    #         return user
-    #     else:
-    #         raise forms.ValidationError('Invalid login credentials')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

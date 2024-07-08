acbea1d1ac8665e86c370cfbd281271be33b9dba
Pedram <pedisdi@gmail.com>
2024-07-08 19:12:11 +0330

    fix: change 'login' and 'register' template and 'login' form based on fbv to cbv conversion

--- src/config/settings.py
+++ src/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib import messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # 3rd party apps
-    "debug_toolbar",  # django-debug-toolbar
+    # "debug_toolbar",  # django-debug-toolbar
     "crispy_forms",
     "crispy_bootstrap4",
 
@@ -142,8 +143,18 @@ INTERNAL_IPS = [
 ]
 
 AUTH_USER_MODEL = "user.User"
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'accounts.auth_backends.AMirzaBackend',
 ]
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-secondary',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}

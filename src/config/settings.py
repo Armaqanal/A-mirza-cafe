d59ebd4b351a50194a349165ac1d7081291df241
ZEYNAB <zibasoltani73@gmail.com>
2024-07-11 12:28:47 +0330

    commit to branch  / ziba

--- src/config/settings.py
+++ src/config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # 3rd party apps
-    "debug_toolbar",  # django-debug-toolbar
+    # "debug_toolbar",  # django-debug-toolbar
 
     # my apps
     'menu',
